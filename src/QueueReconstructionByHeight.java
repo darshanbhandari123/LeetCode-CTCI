686d15a66c8f2e3b67771bda258a5844a1f2ce23
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-05-20 23:51:06 -0400

    Revised 15 out of 16 (Queue reconstruction)

--- src/QueueReconstructionByHeight.java
+++ src/QueueReconstructionByHeight.java
@@ -2,48 +2,39 @@ import java.util.*;
 
 public class QueueReconstructionByHeight {
 
-    static class PairComp implements Comparator<int[]> {
-        public int compare(int[] p1, int[] p2) {
-            int comp_h = Integer.compare(p2[0], p1[0]);
-            return comp_h == 0 ? Integer.compare(p1[1], p2[1]) : comp_h;
-        }
-    }
-    public static void main(String args[])
-    {
-        int people[][] = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
-        int ans[][] = new int[people.length][people[0].length];
+   static class Comp implements Comparator<int []>
+   {
+       public int compare(int[] p1,int[] p2)
+       {
+           int comp = Integer.compare(p2[0],p1[0]);
+           return comp == 0? Integer.compare(p1[1],p2[1]): comp;
+       }
+   }
 
-        LinkedList<int[]> list = new LinkedList();
-        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(1, new PairComp() );
-        for (int[] ppl: people){
-            queue.offer( ppl );
-        }
+   public static void main(String args[])
+   {
+       int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+       PriorityQueue<int[]> queue = new PriorityQueue(1,new Comp());
 
+       for (int[] x:people)
+       {
+           queue.offer(x);
+       }
 
-        while ( ! queue.isEmpty() ) {
-            int[] pair = queue.poll();
-            list.add(pair[1], pair);
-        }
+       List<int[]> result = new ArrayList<>();
+       while(!queue.isEmpty())
+       {
+           int a[] = queue.poll();
+           result.add(a[1],a);
+       }
 
-        int count = 0;
-        for(int x[]:list)
-        {
-            for(int i=0;i<x.length;i++)
-            {
-                ans[count][i] = x[i];
-            }
+       for (int[] x:result) {
 
-            count++;
-        }
+           for (int ans:x){
+               System.out.print(ans + " ");
+           }
 
-        for(int i=0;i<ans.length;i++)
-        {
-            for(int j=0;j<ans[0].length;j++)
-            {
-                System.out.print(ans[i][j]+" ");
-            }
-
-            System.out.println();
-        }
-    }
+           System.out.println();
+       }
+   }
 }
