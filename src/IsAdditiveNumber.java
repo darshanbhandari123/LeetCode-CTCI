59f69ecdd36485cd418839d868ca0cf0e5e625d3
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-05-29 01:13:59 -0400

    Is Additive Number (medium problem) completely solved with all test cases

--- src/IsAdditiveNumber.java
+++ src/IsAdditiveNumber.java
@@ -1,9 +1,11 @@
+import java.math.BigInteger;
+
 public class IsAdditiveNumber {
 
     public static void main(String args[])
     {
         String str1 = "1123581321345589144";
-        String str2 = "111111";
+        String str2 = "0121224";
 
         boolean flag1 = isAdditive(str1);
         boolean flag2 = isAdditive(str2);
@@ -14,18 +16,38 @@ public class IsAdditiveNumber {
 
     private static boolean isAdditive(String str)
     {
-        char c[] = str.toCharArray();
-        for(int i=0;i<c.length;i++)
-            if(c[i]<'0' || c[i]>'9')
-                return false;
+        int n = str.length();
+        for (int i=1;i<=n/2;i++)
+        {
+            for (int j =1; Math.max(i,j)<=n-i-j;j++)
+            {
+                if(isValid(i,j,str))
+                    return true;
+            }
+        }
 
+        return false;
+    }
 
+    private static boolean isValid(int i,int j, String str)
+    {
 
-        for(int i=2;i<c.length;i++)
-        {a
-            int sum = (c[i-2]-'0')+(c[i-1]-'0');
+        if(str.charAt(0) == '0' && i>1)
+            return false;
+        if(str.charAt(i) == '0' && j>1)
+            return false;
 
-            if(sum!=c[i]-'0')
+        String sum = "";
+        BigInteger a = new BigInteger(str.substring(0,i));
+        BigInteger b = new BigInteger(str.substring(i,i+j));
+
+        for (int start = i+j;start<str.length();start+=sum.length())
+        {
+            b = b.add(a);
+            a = b.subtract(a);
+
+            sum = b.toString();
+            if(!str.startsWith(sum,start))
                 return false;
         }
 
