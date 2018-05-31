97b57a487bc492044c6f4305957cb4a9ab243d30
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-05-31 17:59:55 -0400

    Gas Station problem accepted solution

--- src/GasStation.java
+++ src/GasStation.java
@@ -2,8 +2,8 @@ public class GasStation {
 
     public static void main(String args[])
     {
-        int gas[] = {1,2,3,4,5};
-        int cost[] = {3,4,5,1,2};
+        int gas[] = {5,1,2,3,4};
+        int cost[] = {4,4,1,5,1};
 
         int ans = canCompleteCircle(gas,cost);
 
@@ -12,6 +12,9 @@ public class GasStation {
 
     private static int canCompleteCircle(int gas[],int cost[])
     {
+        if(gas.length!=cost.length)
+            return -1;
+
         int gasSum = 0;
         int costSum = 0;
 
@@ -27,12 +30,28 @@ public class GasStation {
         {
             for (int i =0;i<gas.length;i++)
             {
-                if(gas[i]>cost[i])
+                boolean flag = checkGasLeft(gas,cost,i);
+                if(flag)
                     return i;
             }
         }
 
         return -1;
+    }
+
+    private static boolean checkGasLeft(int gas[],int cost[],int i)
+    {
+        int index = i;
+        int gasLeft = 0;
+        for(int j = 0;j<gas.length;j++)
+        {
+            gasLeft += (gas[(index)%gas.length]-cost[(index)%gas.length]);
+            index++;
+            if(gasLeft<0)
+                return false;
+        }
+
+        return true;
     }
 
 }
