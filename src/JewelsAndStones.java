a5366e02adc22ae650227e22635ead7765bc8721
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-06-11 18:33:10 -0400

    Jewels and Stones, Poorpigs

--- src/JewelsAndStones.java
+++ src/JewelsAndStones.java
@@ -1,2 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class JewelsAndStones {
+
+    public static void main(String args[]){
+
+        String j = "z";
+        String s = "ZZ";
+
+        int ans = numJewelsInStones(j,s);
+        System.out.print(ans);
+    }
+
+    private static int numJewelsInStones(String J, String S){
+
+        if(J.length() == 0 || S.length()==0)
+            return 0;
+
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i=0;i<J.length();i++){
+            set.add(J.charAt(i));
+        }
+
+        for (int i=0;i<S.length();i++){
+            if(set.contains(S.charAt(i)))
+                count++;
+        }
+
+        return count;
+    }
+
 }
--- src/PoorPigs.java
+++ src/PoorPigs.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class PoorPigs {
+
+    public static void main(String args[]){
+        int buckets = 1000;
+        int minutesToDie = 30;
+        int minutesToTest = 60;
+
+        int ans = poorPigs(buckets,minutesToDie,minutesToTest);
+        System.out.print(ans);
+    }
+
+    private static int poorPigs(int buckets,int minutesToDie,int minutesToTest){
+        int pigs = 0;
+        int rowcol = minutesToTest/minutesToDie + 1;
+        while (Math.pow(rowcol,pigs)<buckets)
+            pigs++;
+
+        return pigs;
+    }
+
 }
