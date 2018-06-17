1b73e54f075738d86fbfd71f36d1fc7d8492a514
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-06-17 18:05:08 -0400

    sort linkedlist using merge sort

--- src/SortList.java
+++ src/SortList.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
-
 public class SortList {
+
+    static ListNode head;
+
+    public static void main(String args[]){
+        head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode ans = sortList(head);
+        while (ans!=null){
+
+            System.out.print(ans.val + "    ");
+            ans = ans.next;
+
+        }
+    }
+
+    private static ListNode sortList(ListNode head){
+
+        if(head==null || head.next==null)
+            return head;
+
+        ListNode prev=null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null)
+        {
+            prev=slow;
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        prev.next=null;
+        fast=head;
+
+        ListNode left = sortList(fast);
+        ListNode right = sortList(slow);
+
+        return mergeSort(left,right);
+
+    }
+
+    private static ListNode mergeSort(ListNode left,ListNode right){
+
+        ListNode ans = new ListNode(0);
+        ListNode prev = ans;
+
+        while (left!=null && right!=null){
+            ListNode cur;
+            if(left.val < right.val){
+                cur = new ListNode(left.val);
+                left = left.next;
+            }
+            else {
+                cur = new ListNode(right.val);
+                right = right.next;
+            }
+
+            ans.next = cur;
+            ans = ans.next;
+
+        }
+        if (left!=null)
+            ans.next = left;
+
+        if (right!=null)
+            ans.next = right;
+
+        return prev.next;
+    }
 }
