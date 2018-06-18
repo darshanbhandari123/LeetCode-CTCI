0860a3764db5276b7e993dcbe9f23c9581b9b5a1
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-06-18 16:31:23 -0400

    Product of array except self without division

--- src/ArrayOfProductExceptSelf.java
+++ src/ArrayOfProductExceptSelf.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class ArrayOfProductExceptSelf {
+
+    public static void main(String args[]){
+
+        int nums[] = {1,4,6,10};
+
+        int res[] = findProduct(nums);
+        for (int x:res) {
+            System.out.print(x+" ");
+        }
+    }
+
+    private static int[] findProduct(int[] nums){
+
+        int output[] = new int[nums.length];
+        output[0] = 1;
+
+        for (int i=1;i<nums.length;i++){
+            output[i] = output[i-1]*nums[i-1];
+        }
+        int right = 1;
+        for (int i=nums.length-1;i>-1;i--){
+
+            output[i] = output[i] * right;
+            right = right * nums[i];
+
+        }
+
+        return output;
+    }
+
 }
