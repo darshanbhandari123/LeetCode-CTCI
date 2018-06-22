3dce29a3e67136aafdca8aa133fcf47d6409ef4e
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-06-22 17:45:10 -0400

    Maximum product subarray

--- src/MaximumProductSubarray.java
+++ src/MaximumProductSubarray.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class MaximumProductSubarray {
+
+    public static void main(String args[]){
+
+        int nums[] = {2,3,-2,4,-6};
+        int max = nums[0];
+        int max_ending_here = nums[0];
+        int min_ending_here = nums[0];
+
+        for (int i=1;i<nums.length;i++){
+
+            if(nums[i]<0){
+                int temp = max_ending_here;
+                max_ending_here = min_ending_here;
+                min_ending_here = temp;
+            }
+
+            max_ending_here = Math.max(nums[i],max_ending_here*nums[i]);
+            min_ending_here = Math.min(nums[i],min_ending_here*nums[i]);
+
+            if(max_ending_here>max)
+                max = max_ending_here;
+
+        }
+
+        System.out.println(max);
+    }
+
 }
