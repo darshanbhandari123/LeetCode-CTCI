34e07d53ad93c7cd8eedeedcd435cdf84146cd1c
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-09-27 21:13:16 -0400

    Facebook Questions

---
+++ src/MaxSizeSubarraySumEqualsK.java
@@ -0,0 +1,9 @@
+public class MaxSizeSubarraySumEqualsK {
+
+    public static void main(String args[]){
+
+        
+
+    }
+
+}
--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -1,2 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class TaskScheduler {
+
+    public static void main(String args[]){
+
+        char[] tasks = {'A','A','A','B','B','B'};
+        int n = 2;
+
+        int ans = leastInterval(tasks,n);
+        System.out.println(ans);
+
+    }
+
+    private static int leastInterval(char[] tasks,int n){
+
+        int c[] = new int[26];
+        for (char x:
+             tasks) {
+            c[x-'A']++;
+        }
+
+        Arrays.sort(c);
+
+        int i=25;
+        while (i>-1 && c[i]==c[25]){
+            i--;
+        }
+
+        return Math.max(tasks.length,((c[25]-1)*(n+1)) + 25-i);
+
+    }
+
 }
